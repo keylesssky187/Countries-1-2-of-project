2b52992a5275ef8df97c5f3079b30bda29368e4c
keylesssky187 <60062880+keylesssky187@users.noreply.github.com>
2020-02-16 15:22:06 +0300

    1/2project Countries ready

--- server/index.js
+++ server/index.js
@@ -5,4 +5,23 @@ let mc = require("mongodb").MongoClient, url = "mongodb://localhost";
 let db;
 
 app.use(express.static("../client"));
-app.listen(8000);
+
+mc.connect(url, function(err, client){
+	if (err){
+		console.log(err.stack);
+	}
+	db = client.db("countries");
+	app.listen(8000);
+	getArray();
+});
+
+function getArray(){
+	db.collection("areas").find({}).toArray(
+		function(err, doc){
+			if (err) console.log(err.stack)
+            for (country of doc){
+            	console.log(country.name, country.area, country.en)
+            }
+		});
+}
+
